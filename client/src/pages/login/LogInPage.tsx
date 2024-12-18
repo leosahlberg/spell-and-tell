526fb4e21d718a5a8903390d1273f797f7d80946
isabellaes <borjessonisabella@gmail.com>
2024-12-18 14:34:01 +0100

    add login form

--- client/src/pages/login/LogInPage.tsx
+++ client/src/pages/login/LogInPage.tsx
@@ -1,5 +1,25 @@
+import { TextField } from "@mui/material";
+import style from "./logInPage.module.scss";
+
 const LogInPage = () => {
-  return <div>Login</div>;
+  return (
+    <div className={style.container}>
+      <form action="submit" className={style.form}>
+        <TextField
+          id="outlined-basic"
+          label="Användarnamn"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Lösenord"
+          variant="outlined"
+          color="primary"
+        />
+        <button className={style.btn}>Logga in</button>
+      </form>
+    </div>
+  );
 };
 
 export default LogInPage;
