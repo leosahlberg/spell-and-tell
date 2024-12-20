b77727d3aafeb2de51ae878e2fbb4760b5a3ae63
isabellaes <borjessonisabella@gmail.com>
2024-12-20 10:38:53 +0100

    style fixed loginpage

--- client/src/pages/login/LogInPage.tsx
+++ client/src/pages/login/LogInPage.tsx
@@ -1,5 +1,6 @@
 import { Paper, TextField, Typography } from "@mui/material";
 import style from "./logInPage.module.scss";
+import Button from "../../components/buttons/Button";
 
 const LogInPage = () => {
   return (
@@ -17,7 +18,7 @@ const LogInPage = () => {
         <Typography variant="h4" gutterBottom>
           Logga in
         </Typography>
-        <form action="submit" className={style.form}>
+        <form onSubmit={() => console.log("loggar in")}>
           <TextField
             label="Användarnamn"
             variant="outlined"
@@ -33,78 +34,8 @@ const LogInPage = () => {
             fullWidth
             required
           />
-          <button className={style.btn}>Logga in</button>
-        </form>
-      </Paper>
-    </div>
-  );
-};
-
-export default LogInPage;
-
-/* 
-
-import styles from "./registrationPage.module.scss";
-import {
-  TextField,
-  Checkbox,
-  FormControlLabel,
-  Typography,
-  Paper,
-} from "@mui/material";
-import { Link, useNavigate } from "react-router";
-import Button from "../../components/buttons/Button";
-
-const RegistrationPage = () => {
-  const navigate = useNavigate();
-
-  const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    navigate("home");
-  };
-
-  return (
-    <div className={styles.registration}>
-      <Paper elevation={24} sx={{ padding: 6, maxWidth: 800, maxHeight: "100vh", marginBottom: 20,  border: "2px solid rgb(158, 186, 158)", }}>
-        <Typography variant="h4" gutterBottom>
-          Skapa ett användarkonto
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            sx={{ marginBottom: 4, marginTop: 4 }}
-            label="Användarnamn"
-            fullWidth
-            required
-          />
-          <TextField
-            sx={{ marginBottom: 4 }}
-            label="Email"
-            fullWidth
-            required
-          />
-          <TextField
-            sx={{ marginBottom: 4 }}
-            label="Lösenord"
-            type="password"
-            fullWidth
-            required
-          />
-             <TextField
-            sx={{ marginBottom: 4 }}
-            label="Bekräfta Lösenord"
-            type="password"
-            fullWidth
-            required
-          />
-          <FormControlLabel
-            control={<Checkbox />}
-            label="Jag accepterar villkoren och regler"
-          />
-          <p>(läs <Link to="/home"> villkor och regler</Link>)</p>
           <div style={{ display: "flex", justifyContent: "flex-end" }}>
-            <Button
-              text="Skapa"
-            ></Button>
+            <Button text="Logga in"></Button>
           </div>
         </form>
       </Paper>
@@ -112,7 +43,4 @@ const RegistrationPage = () => {
   );
 };
 
-export default RegistrationPage;
-
-
-*/
+export default LogInPage;
