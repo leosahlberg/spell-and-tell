ec49a00dee3da16d737693a9a9a6e5f40b8703ba
isabellaes <borjessonisabella@gmail.com>
2024-12-20 15:39:56 +0100

    add homepage

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,5 +1,63 @@
+import { Paper, TextField, Typography } from "@mui/material";
+import style from "./homePage.module.scss";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div className={style.container}>
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 6,
+          marginBottom: 2,
+          width: "45%",
+          margin: 1,
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Mina berättelser
+        </Typography>
+      </Paper>
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 6,
+          marginBottom: 2,
+          width: "45%",
+          margin: 1,
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Hitta berättelse att bidra till
+        </Typography>
+      </Paper>
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 6,
+          marginBottom: 2,
+          width: "45%",
+          margin: 1,
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Nya förfrågningar
+        </Typography>
+      </Paper>
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 6,
+          marginBottom: 2,
+          width: "45%",
+          margin: 1,
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Mina bidrag
+        </Typography>
+      </Paper>
+    </div>
+  );
 };
 
 export default HomePage;
