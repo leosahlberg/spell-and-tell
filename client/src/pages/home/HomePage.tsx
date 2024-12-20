a625198535c5a2b818e63c430205107a84d81e75
isabellaes <borjessonisabella@gmail.com>
2024-12-20 17:25:40 +0100

    add home page

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,7 +1,11 @@
 import { Paper, TextField, Typography } from "@mui/material";
 import style from "./homePage.module.scss";
+import image from "../../assets/backgroundletters.png";
+import { useNavigate } from "react-router";
 
 const HomePage = () => {
+  const navigation = useNavigate();
+
   return (
     <div className={style.container}>
       <Paper
@@ -11,12 +15,11 @@ const HomePage = () => {
           marginBottom: 2,
           width: "45%",
           margin: 1,
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
         }}
-      >
-        <Typography variant="h5" gutterBottom>
-          Mina berättelser
-        </Typography>
-      </Paper>
+      ></Paper>
       <Paper
         elevation={4}
         sx={{
@@ -24,11 +27,44 @@ const HomePage = () => {
           marginBottom: 2,
           width: "45%",
           margin: 1,
+
+          backgroundColor: "rgb(207, 207, 236)",
+          display: "flex",
+          justifyContent: "right",
+          flexDirection: "column",
         }}
       >
-        <Typography variant="h6" gutterBottom>
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            backgroundColor: "white",
+            padding: 1,
+            width: "fit-content",
+            fontWeight: "500",
+            height: "fit-content",
+          }}
+        >
           Hitta berättelse att bidra till
         </Typography>
+        <Typography variant="body1" gutterBottom>
+          Vill du inte starta en ny berättelse?
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Utforska öppna berättelser som du kan bidra till. Tänk på att läsa
+          igenom berättelsen och vilka villkor som gäller för just den du valt
+          innan du börjar!
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          Glöm inte att läsa våra regler och villkor också. De hittar du{" "}
+          <span className={style.link} onClick={() => navigation("/terms")}>
+            här!
+          </span>
+        </Typography>
+        <button className={style.button} onClick={() => navigation("/storys")}>
+          <h2>Utforska berättelser</h2>
+        </button>
       </Paper>
       <Paper
         elevation={4}
@@ -37,11 +73,40 @@ const HomePage = () => {
           marginBottom: 2,
           width: "45%",
           margin: 1,
+
+          backgroundColor: "rgb(207, 207, 236)",
+          display: "flex",
+          justifyContent: "right",
+          flexDirection: "column",
         }}
       >
-        <Typography variant="h6" gutterBottom>
-          Nya förfrågningar
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            backgroundColor: "white",
+            padding: 1,
+            width: "fit-content",
+            fontWeight: "500",
+            height: "fit-content",
+          }}
+        >
+          Hur funkar det?
         </Typography>
+        <Typography variant="body1" gutterBottom>
+          Spell & Tell är ett berättar spel för dig och dina vänner. Det funkar
+          lika bra att använda i skolan som på fritiden.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Skapa roliga och kreativa berättelser tillsammans. Du startar en
+          berättelse och väljer vilka regler du vill applicera, sen skickar du
+          den vidare till en person eller lägger den öppen för vem som helst att
+          bidra.
+        </Typography>
+
+        <button className={style.button} onClick={() => navigation("/terms")}>
+          <h2>Läs regler och villkor</h2>
+        </button>
       </Paper>
       <Paper
         elevation={4}
@@ -50,12 +115,11 @@ const HomePage = () => {
           marginBottom: 2,
           width: "45%",
           margin: 1,
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
         }}
-      >
-        <Typography variant="h6" gutterBottom>
-          Mina bidrag
-        </Typography>
-      </Paper>
+      ></Paper>
     </div>
   );
 };
