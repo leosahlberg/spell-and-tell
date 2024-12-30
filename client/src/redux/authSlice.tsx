b3a64d759a8b5f7bda7f623d1803c82712eafa4a
isabellaes <borjessonisabella@gmail.com>
2024-12-30 15:29:09 +0100

    update slice and store

--- client/src/redux/authSlice.tsx
+++ client/src/redux/authSlice.tsx
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "../utils/types";
+import { logIn } from "../utils/api";
 
 type InitialStateType = {
   user: User | null;
@@ -13,18 +14,46 @@ const initialState: InitialStateType = {
   registerAccepted: null,
 };
 
+type LogInData = {
+  username: string;
+  password: string;
+};
+
+type LogInResponse = {
+  user: User;
+  token: string;
+};
+
+export const getUserToken = createAsyncThunk<
+  LogInResponse,
+  LogInData,
+  { rejectValue: string }
+>("token/getUserToken", async ({ username, password }, { rejectWithValue }) => {
+  try {
+    const response = await logIn(username, password);
+    return response;
+  } catch (error) {
+    return rejectWithValue("Something went wrong");
+  }
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    logIn: (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-    },
     logout: (state, action) => {
       state.user = null;
       state.token = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      getUserToken.fulfilled,
+      (state, action: PayloadAction<LogInResponse>) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      }
+    );
   },
 });
 
