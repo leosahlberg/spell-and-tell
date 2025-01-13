d19161718dfa9d8696c1354f2d780d6f98bd114b
isabellaes <borjessonisabella@gmail.com>
2025-01-13 17:57:29 +0100

    add rouleset model and router

--- server/src/database/models/roulesetModel.ts
+++ server/src/database/models/roulesetModel.ts
@@ -1,25 +1,22 @@
 import mongoose from "mongoose";
 
 const rouleSetSchema = new mongoose.Schema({
-  maxNumberOfWords: {
+  storyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Story",
+  },
+  maxNumberOfWordsPerContribution: {
     type: String,
   },
   numberOfContributors: {
     type: Number,
   },
-  assignedContributors: [{
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    }
-  }],
   scoring: {
-    type: Boolean
+    type: Boolean,
   },
   spellChecking: {
-    type: Boolean
-  }
-
+    type: Boolean,
+  },
 });
 
 export const rouleSetModel = mongoose.model("RouleSet", rouleSetSchema);
