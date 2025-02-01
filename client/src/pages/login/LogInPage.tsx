3e60bdb7f4b090b86c3b7dcff834d24cc7cf1c5f
Eva-Li80 <evaliboden@gmail.com>
2025-02-01 12:09:20 +0100

    loggin update styling and accessibility

--- client/src/pages/login/LogInPage.tsx
+++ client/src/pages/login/LogInPage.tsx
@@ -1,10 +1,11 @@
 import { Paper, TextField, Typography } from "@mui/material";
 import style from "./logInPage.module.scss";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store";
 import { fetchLogin } from "../../redux/authSlice";
+import backgroundImage from "../../assets/bookimg.jpg"
 
 const LogInPage = () => {
   const [username, setUsername] = useState("");
@@ -13,7 +14,6 @@ const LogInPage = () => {
 
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
-
   const data = useSelector<RootState>((state) => state.auth.error) as string;
 
   useEffect(() => {
@@ -21,18 +21,28 @@ const LogInPage = () => {
   }, [data]);
 
   return (
-    <div className={style.container}>
+    <div className={style.container}
+    style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      maxHeight: "100%",
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
       <Paper
         elevation={24}
         sx={{
           padding: 6,
-          maxWidth: 800,
-          maxHeight: "100vh",
-          marginBottom: 2,
-          border: "2px solid rgb(158, 186, 158)",
+          maxWidth: 600,
+          maxHeight: 500,
+          border: "2px solid rgb(195, 158, 121)",
+          backgroundColor: "#FFFEFA",
         }}
       >
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h1" gutterBottom className={style.loggin}>
           Logga in
         </Typography>
         <form
@@ -46,35 +56,68 @@ const LogInPage = () => {
             }
           }}
         >
+          <label className={style.label} id="username-label" htmlFor="username">
+            Användarnamn
+          </label>
           <TextField
-            label="Användarnamn"
             variant="outlined"
-            sx={{ marginBottom: 4, marginTop: 4 }}
+            slotProps={{
+              input: {
+                style: {
+                  color: "black",
+                  fontStyle: "italic",
+                },
+              },
+            }}
+            sx={{ marginBottom: 4, marginTop: 1 }}
             fullWidth
             required
+            id="username"
+            placeholder="Ange användarnamn (exempel: Anna79)"
             onChange={(e) => setUsername(e.currentTarget.value)}
           />
+
+          <label className={style.label} id="password-label" htmlFor="password">
+            Lösenord
+          </label>
           <TextField
-            sx={{ marginBottom: 4 }}
-            label="Lösenord"
+            slotProps={{
+              input: {
+                style: {
+                  color: "black",
+                  fontStyle: "italic",
+                },
+              },
+            }}
             variant="outlined"
             type="password"
+            id="password"
+            placeholder="Ange lösenord (minst 8 tecken)"
             onChange={(e) => setPassword(e.currentTarget.value)}
             fullWidth
             required
           />
+
           {error != null ? <p style={{ color: "red" }}>{error}</p> : <></>}
-          <div style={{ display: "flex", justifyContent: "flex-end" }}>
-            <input className={style.button} type="submit" value="Logga in" />
+          <div style={{  display: "flex", justifyContent: "center", width: "100%", marginTop: "20px"  }}>
+            <button className={style.button} type="submit">
+              Logga in
+            </button>
           </div>
         </form>
       </Paper>
-      <Typography variant="h6" gutterBottom>
-        Har du inget konto? Registrera dig{" "}
-        <span className={style.link} onClick={() => navigate("/registration")}>
-          här
-        </span>
-      </Typography>
+      <section>
+        <Typography variant="h2" gutterBottom className={style.register}>
+          Har du inget konto?
+          <Link
+            className={style.link}
+            to={"/registration"}
+            aria-label="Registrera ett konto"
+          >
+            Registrera dig här
+          </Link>
+        </Typography>
+      </section>
     </div>
   );
 };
